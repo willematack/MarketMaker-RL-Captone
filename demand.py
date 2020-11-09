519375077de98a33670553744defe25910893ce7
Spencer <16sek@queensu.ca>
2020-11-09 11:43:34 -0500

    Started on a QLAgent and added more comments

--- demand.py
+++ demand.py
@@ -1,7 +1,20 @@
-import numpy.random as np
+import numpy.random as npr
+import numpy as np
 import matplotlib.pyplot as plt
 
 def demand(Size):
-    buy = np.standard_normal(size=int(Size))*50
-    sell = np.standard_normal(size=int(Size))*50
+    """
+    buyfl/sellfl:  generates normally distributed array of floats. multiply by 100 to
+                    get values around 100.
+
+    buy/sell:    take absolute value of each value in buyfl and modulo 101 to ensure
+                all value between 0 and 100. It also makes these values ints.
+                This is a rough way fo doing this that causes the values to no
+                longer be normally distributed, so this may be adjusted.
+
+    """
+    buyfl = npr.randn(int(Size))*100        #generates normally distributed array
+    buy = np.array([int(abs(x)%101) for x in buyfl])
+    sellfl = npr.randn(int(Size))*100
+    sell = np.array([int(abs(x)%100) for x in sellfl])
     return buy, sell
--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from demand import demand
 from agent import Agent
 import numpy as np
 import matplotlib.pyplot as plt
+
 def makeAgents(agent_num):
+    """
+    agents: an array of Agent()'s of length agent_num
+    """
     agents = [Agent() for i in range(agent_num)]
     return agents
 
 def initialize(step, time, drift, volatility, initValue):
+    """
+    Price:  an array from time 0 to 'time' with increments of value 'step'. thus,
+            it is of length time/step. Drift and volatility are the two parameters
+            that geometric brownian motion needs.
+
+    buy/sellOrders: This will generate an array of integers in between 0 and 100
+                    of length time/step.
+    """
     price = gmbrownian(step, time, drift, volatility, initValue)
     buyOrders, sellOrders = demand(time/step)
     return price, buyOrders, sellOrders
@@ -33,25 +45,31 @@ def simulation():
     initValue = 20
     agent_num = 5
 
+    #get arrays
     price, buyOrders, sellOrders = initialize(step, time, drift, volatility, initValue)
+    #get agents
     agents = makeAgents(agent_num)
+
+    #initialize the bid, ask and spread arrays
     bid = np.zeros(agent_num)
     ask = np.zeros(agent_num)
     spread = np.zeros(agent_num)
 
-    for i in range(len(price)):
-        for j in range(agent_num):
+    for i in range(len(price)):    #loop over every value in the array price
+        for j in range(agent_num):  #For each price get the bid and ask of each agent
             if i == 0:
-                bid[j], ask[j] = agents[j].quote(price[i], 50, 50)
+                bid[j], ask[j] = agents[j].quote(price[i], 50, 50)  #if i==0, have a demand of 50,50
             else:
                 bid[j], ask[j] = agents[j].quote(price[i], buyOrders[i-1], sellOrders[i-1])
-            spread[j] = ask[j] - bid[j]
-        winner = spread.argmin()
+            spread[j] = ask[j] - bid[j] #define the spread for each bid-ask
 
-        agents[winner].cumulative_profit(spread[winner], abs(buyOrders[i]-sellOrders[i]))
+        winner = spread.argmin()    #this returns the index of the tightest spread
+
+        agents[winner].cumulative_profit(spread[winner], abs(buyOrders[i]-sellOrders[i]))   #winner cashes in
 
     for agent in agents:
-        plt.plot(agent.profit)
+        plt.plot(agent.profit)  #plot the agents
+        plt.legend([0,1,2,3,4])
     plt.show()
     return agents
 
